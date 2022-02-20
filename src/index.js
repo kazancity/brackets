98a0aa4cac456b331dab5935ce4ecf710c6b5ae0
Artem Banderov <artem_75@bk.ru>
2022-02-20 21:09:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 module.exports=check=(s,b)=>{m=[];if(s.length%2!==0)return false;for(let i=0;i<s.length;i++){m.push(s[i]);
-  for(let j=0;j<b.length;j++){if(m[m.length-1]==b[j][1]&&m[m.length-2]==b[j][0]){m.splice(0,2)}}}return m.length==0}
+for(let j=0;j<b.length;j++){if(m[m.length-1]==b[j][1]&&m[m.length-2]==b[j][0]){m.pop();m.pop()}}}return m.length==0}
